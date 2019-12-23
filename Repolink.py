2076f5c19cd80b439895f651acb990e36b4b4f53
Yorkin <937697555@qq.com>
2019-12-23 23:44:41 +0800

    Add searchRepo and searchUser

--- Repolink.py
+++ Repolink.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import sys
+import re
 
 def printHelp():
     print('''
@@ -9,9 +10,16 @@ def printHelp():
     Repolink search -u <user-name>
     Repolink search -r <repo-name>
     ''')
-    
-def searchRepo(reponame):
-    
+
+def searchUser(username,page):
+    res = urllib.request.urlopen('https://github.com/search?p='+str(page)+'&q='+username+'&type=Users')    
+    items = re.findall('(?<=/users/).+?(?=/hovercard)',str(res.read()))
+    return items
+
+def searchRepo(reponame,page):
+    res = urllib.request.urlopen('https://github.com/search?p='+str(page)+'&q='+reponame)
+    items = re.findall('(?<=<a class="muted-link" href="/).+?(?=/stargazers">)',str(res.read()))
+    return items
 
 def getRepoByFullName(fullname):
     url='https://api.github.com/repos/'+fullname
@@ -68,10 +76,27 @@ if __name__ == "__main__":
                 print('\033[32m' + str(items[index]['stargazers']) + ' Stars   ' + str(items[index]['forks']) + ' Forks   ' +str(items[index]['watchers'])+' Watchers\033[0m')
                 print(items[index]['clone_url'])
                 print()
-        # elif cmd == 'search':
-        #     if falg == '-u':
 
-        #     else:
+        elif cmd == 'search':
+            if flag == '-u':
+                page = 1
+                while(True):
+                    print('Repolink: Searching ...')
+                    for item in searchUser(arg,page):
+                        print('\033[33m' + item + '\033[0m','https://github.com/' + item)
+                    if(input('Press Enter to continue searching.')!=''):break
+                    page+=1
+
+            elif flag == '-r':
+                page = 1
+                while(True):
+                    print('Repolink: Searching ...')
+                    for item in searchRepo(arg,page):
+                        print('\033[33m' + item + '\033[0m','https://github.com/' + item + '.git')
+                        if(input('Press Enter to continue searching.')!=''):break
+                        page+=1
+            else:
+                printUndefine
         else:
             printUndefine
 
